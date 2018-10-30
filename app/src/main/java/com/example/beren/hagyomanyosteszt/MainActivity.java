21305598a8baba16951ed2703788cd033e8ab92a
Kikikan <kikikkan@freemail.hu>
2018-10-30 19:32:54 +0100

    gradle update + switch átírása

--- app/src/main/java/com/example/beren/hagyomanyosteszt/MainActivity.java
+++ app/src/main/java/com/example/beren/hagyomanyosteszt/MainActivity.java
@@ -9,29 +9,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
+
         TextView t1;
         Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b7=(Button)findViewById(R.id.szam7);
-        b8=(Button)findViewById(R.id.szam8);
-        b9=(Button)findViewById(R.id.szam9);
-        b4=(Button)findViewById(R.id.szam4);
-        b5=(Button)findViewById(R.id.szam5);
-        b6=(Button)findViewById(R.id.szam6);
-        b1=(Button)findViewById(R.id.szam1);
-        b2=(Button)findViewById(R.id.szam2);
-        b3=(Button)findViewById(R.id.szam3);
+        b7 = findViewById(R.id.szam7);
+        b8 = findViewById(R.id.szam8);
+        b9 = findViewById(R.id.szam9);
+        b4 = findViewById(R.id.szam4);
+        b5 = findViewById(R.id.szam5);
+        b6 = findViewById(R.id.szam6);
+        b1 = findViewById(R.id.szam1);
+        b2 = findViewById(R.id.szam2);
+        b3 = findViewById(R.id.szam3);
 
 
-        t1=(TextView)findViewById(R.id.kijelzo1);
+        t1 = findViewById(R.id.kijelzo1);
 
 
         b7.setOnClickListener(this);
@@ -48,79 +46,28 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     public void onClick(View v){
 
+        Button button = (Button) v;
+
         try{
             int proba = v.getId();
 
             switch(proba){
 
-                case R.id.szam7: t1.setText(t1.getText()+"7");
-                break;
-
-                case R.id.szam8: t1.setText(t1.getText()+"8");
+                case R.id.szam1:
+                case R.id.szam2:
+                case R.id.szam3:
+                case R.id.szam4:
+                case R.id.szam5:
+                case R.id.szam6:
+                case R.id.szam7:
+                case R.id.szam8:
+                case R.id.szam9:
+                    t1.setText(t1.getText() + button.getText().toString());
                     break;
-
-                case R.id.szam9: t1.setText(t1.getText()+"9");
-                    break;
-
-                case R.id.szam4: t1.setText(t1.getText()+"4");
-                    break;
-
-                case R.id.szam5: t1.setText(t1.getText()+"5");
-                    break;
-
-                case R.id.szam6: t1.setText(t1.getText()+"6");
-                    break;
-
-                case R.id.szam1: t1.setText(t1.getText()+"1");
-                    break;
-
-                case R.id.szam2: t1.setText(t1.getText()+"2");
-                    break;
-
-                case R.id.szam3: t1.setText(t1.getText()+"3");
-                    break;
-
-
             }
-
-
-
-
-
-
-
-
-
-
-
         }
-
-
         catch(Exception e){
             Toast.makeText(getApplicationContext(), "Számot kell  begépelni...", Toast.LENGTH_SHORT).show();
-
-
-
-
-
-
-
-
-
-
-
         }
-
-
-
-
-
-
-
-
     }
-
-
-
-
 }
